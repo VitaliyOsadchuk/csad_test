ce0ebac561a5e23a28aec7cc8725baa8c1171331
Vitaliy <145994590+VitaliyOsadchuk@users.noreply.github.com>
2024-10-16 22:10:32 +0300

    Update test.ps1

--- ci/test.ps1
+++ ci/test.ps1
@@ -6,18 +6,4 @@ if ($LASTEXITCODE -eq 0) {
     echo "Tests executed successfully."
 } else {
     echo "Failed to execute tests."
-    Get-Content "ci/test_output.txt" | Write-Output
-    exit 1
-}
-
-# Вивід вмісту тестового файлу для журналювання
-echo "Test output content:"
-Get-Content "ci/test_output.txt" | Write-Output
-
-# Переконайтеся, що створений файл існує та не порожній
-if ((Get-Item "ci/test_output.txt").length -gt 0) {
-    echo "Test output file created successfully and is not empty."
-} else {
-    echo "Test output file is empty or does not exist."
-    exit 1
-}
+    Get-Content "ci/test_output.txt" | Write
