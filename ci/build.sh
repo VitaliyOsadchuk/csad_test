d0d177d06f69b64f5bb1cae3f0c1f1131456a1ad
Vitaliy <145994590+VitaliyOsadchuk@users.noreply.github.com>
2024-10-16 20:11:52 +0300

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,5 +6,20 @@ mkdir -p build
 # Компіляція головного проекту
 cl /EHsc /Fe:build/SW_Task2.exe src/SW_Task2/SW_Task2/client.cpp
 
+# Перевірка результатів компіляції
+if [ $? -eq 0 ]; then
+    echo "Main project compiled successfully."
+else
+    echo "Failed to compile main project."
+    exit 1
+fi
+
 # Переміщення виконуваного файлу тесту до build
 cp tests/test_serial_communication/x64/Debug/test_serial_communication.exe build/
+
+if [ $? -eq 0 ]; then
+    echo "Test executable copied successfully."
+else
+    echo "Failed to copy test executable."
+    exit 1
+fi
