03241e4d578bc5559ba1e27be29f21bcb07d017e
Vitaliy <145994590+VitaliyOsadchuk@users.noreply.github.com>
2024-10-16 22:05:45 +0300

    Update and rename test.sh to test.ps1

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,25 +1,23 @@
-#!/bin/bash
-
 echo "Running test executable..."
-build/test_serial_communication.exe > ci/test_output.txt 2>&1
+Start-Process -FilePath "build/test_serial_communication.exe" -NoNewWindow -Wait -RedirectStandardOutput "ci/test_output.txt" -RedirectStandardError "ci/test_output.txt"
 
 # Перевірка, чи тестовий файл успішно виконано
-if [ $? -eq 0 ]; then
+if ($LASTEXITCODE -eq 0) {
     echo "Tests executed successfully."
-else
+} else {
     echo "Failed to execute tests."
-    cat ci/test_output.txt
+    Get-Content "ci/test_output.txt" | Write-Output
     exit 1
-fi
+}
 
 # Вивід вмісту тестового файлу для журналювання
 echo "Test output content:"
-cat ci/test_output.txt
+Get-Content "ci/test_output.txt" | Write-Output
 
 # Переконайтеся, що створений файл існує та не порожній
-if [ -s ci/test_output.txt ]; then
+if ((Get-Item "ci/test_output.txt").length -gt 0) {
     echo "Test output file created successfully and is not empty."
-else
+} else {
     echo "Test output file is empty or does not exist."
     exit 1
-fi
+}
