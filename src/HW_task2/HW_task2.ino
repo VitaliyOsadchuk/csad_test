d9f891e6c72304b734e5114a2ac1a9d4a3575cfa
VitaliyOsadchuk <vitalii.osadchuk.ki.2021@lpnu.ua>
2024-10-17 18:57:47 +0300

    boost

--- src/HW_task2/HW_task2.ino
+++ src/HW_task2/HW_task2.ino
@@ -7,7 +7,7 @@ void loop() {
         String message = Serial.readStringUntil('\n');  // read until '\n'
         
         if (message.length() > 0) {  // message not empty
-            message += " - modified by HW*";  // modify
+            message += " --- modified by HW";  // modify
             Serial.println(message);  // send
         }
     }
